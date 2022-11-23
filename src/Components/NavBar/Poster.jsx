02d36f23ed030b5efe29af3a9da8701571c6e080
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-23 17:25:35 +0200

    Add background to home page

--- src/Components/NavBar/Poster.jsx
+++ src/Components/NavBar/Poster.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Poster() {
-  return <div className="poster d-flex justify-content-center align-items-center">Poster</div>;
+  return <div className="poster d-flex justify-content-center align-items-center" />;
 }
 
 export default Poster;
