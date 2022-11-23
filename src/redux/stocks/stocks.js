a722998ce38ed4dcd9dfff015b2e93bf918d669a
KareemWilson <kareemmagdywilson@gmail.com>
2022-11-23 17:38:45 +0200

    Delete comments

--- src/redux/stocks/stocks.js
+++ src/redux/stocks/stocks.js
@@ -6,58 +6,14 @@ const initialState = [];
 
 // Calling API
 const getStocksFromAPI = async () => {
-  //   const response = await fetch(
-  //     'https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=ce82bb18d7fa69ee9a1317f3625a1ab3',
-  //   );
-  //   const data = await response.json();
-  const companies = [
-    {
-      symbol: 'AAPL',
-      name: 'Apple Inc.',
-      change: 1.695,
-      price: 149.705,
-      changesPercentage: 1.1451979,
-    },
-    {
-      symbol: 'AMD',
-      name: 'Advanced Micro Devices, Inc.',
-      change: 1.685,
-      price: 74.145,
-      changesPercentage: 2.3254175,
-    },
-    {
-      symbol: 'TSLA',
-      name: 'Tesla, Inc.',
-      change: 1.4293,
-      price: 169.299,
-      changesPercentage: 0.85143626,
-    },
-    {
-      symbol: 'AMZN',
-      name: 'Amazon.com, Inc.',
-      change: 0.6158,
-      price: 93.0758,
-      changesPercentage: 0.66601664,
-    },
-    {
-      symbol: 'F',
-      name: 'Ford Motor Company',
-      change: 0.11,
-      price: 14.06,
-      changesPercentage: 0.7885349,
-    },
-    {
-      symbol: 'CCL',
-      name: 'Carnival Corporation & plc',
-      change: 0.12,
-      price: 9.44,
-      changesPercentage: 1.2875525,
-    },
-  ];
-  //   for (let i = 0; i < 6; i += 1) {
-  //     companies.push(data[i]);
-  //   }
-  console.log(companies);
+  const response = await fetch(
+    'https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=ce82bb18d7fa69ee9a1317f3625a1ab3',
+  );
+  const data = await response.json();
+  const companies = [];
+  for (let i = 0; i < 14; i += 1) {
+    companies.push(data[i]);
+  }
   return companies;
 };
 
